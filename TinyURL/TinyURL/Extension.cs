5dfbae72dc7a2cfb81ac3012f4dafb3db9aa3add
BIGDummyHead <47464673+BIGDummyHead@users.noreply.github.com>
2021-04-14 12:07:33 -0500

    Source Upload

---
+++ TinyURL/TinyURL/Extension.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.IO;
+using System;
+
+namespace TinyURL
+{
+    public static class Extension
+    {
+        public static string Shrink(this string url)
+        {
+            string shrunk = string.Empty;
+            url = "http://tinyurl.com/api-create.php?url=" + url.ToLower();
+
+            HttpWebResponse rep = null;
+            HttpWebRequest o = WebRequest.Create(url) as HttpWebRequest;
+            o.Method = "GET";
+
+            rep = o.GetResponse() as HttpWebResponse;
+
+            using (StreamReader sr = new StreamReader(rep.GetResponseStream()))
+            {
+                shrunk = sr.ReadToEnd();
+                rep.Close();
+                o.Abort();
+            }
+
+            return shrunk;
+        }
+
+        public static string Shrink(this Uri url) => Shrink(url.OriginalString);
+    }
+}
