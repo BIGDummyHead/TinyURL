779435d4ace6f990c43bd40e412c5c68cbe8af33
SBFAM22 <47464673+SBFAM22@users.noreply.github.com>
2021-06-16 14:23:25 -0400

    Gives async code.

--- TinyURL/TinyURL/Extension.cs
+++ TinyURL/TinyURL/Extension.cs
@@ -1,32 +1,27 @@
 ﻿using System.Net;
 using System.IO;
 using System;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TinyURL
 {
     public static class Extension
     {
-        public static string Shrink(this string url)
+        public static async Task<string> ShrinkAsync(this string url)
         {
-            string shrunk = string.Empty;
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                throw new HttpRequestException("The URL you provided is invalid, it should be formatted to be Absolute.");
+
             url = "http://tinyurl.com/api-create.php?url=" + url;
 
-            HttpWebResponse rep = null;
-            HttpWebRequest o = WebRequest.Create(url) as HttpWebRequest;
-            o.Method = "GET";
-
-            rep = o.GetResponse() as HttpWebResponse;
-
-            using (StreamReader sr = new StreamReader(rep.GetResponseStream()))
-            {
-                shrunk = sr.ReadToEnd();
-                rep.Close();
-                o.Abort();
-            }
-
-            return shrunk;
+            return await (await ApiRequester.RequestAPIAsync(url, RequestType.Get)).Content.ReadAsStringAsync();
         }
 
-        public static string Shrink(this Uri url) => Shrink(url.OriginalString);
+        public static async Task<string> ShrinkAsync(this Uri url) => await ShrinkAsync(url.OriginalString);
+
+        public static string Shrink(this string url) => ShrinkAsync(url).Result;
+        public static string Shrink(this Uri url) => ShrinkAsync(url).Result;
     }
+
 }
